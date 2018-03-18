3657abd86b851b7b2c01e817271972676562c15d
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-03-18 15:03:52 -0400

    rearranged methods & passing some tests

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,28 +2,53 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ItemParser {
 
 
-    public ArrayList<String> parseRawDataIntoStringArray(String rawData){
-        String stringPattern = "##";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawData);
-        return response;
-    }
-
-    public Item parseStringIntoItem(String rawItem) throws ItemParseException{
-        return null;
-    }
-
-    public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
-        return response;
-    }
-
-    private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString){
+    // 1
+    private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString) {
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
+    }
+
+    // 2
+    public ArrayList<String> parseRawDataIntoStringArray(String rawData) {
+        String stringPattern = "##";
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern, rawData);
+        return response;
+    }
+
+    // 3
+    public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem) {
+        String stringPattern = "[@|^|*|%|!|;]";
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern, rawItem);
+        return response;
+    }
+
+    // 4
+    public Item parseStringIntoItem(String rawItem) throws ItemParseException {
+        int count = 0;
+        String strPrice = "\\d.\\d\\d";
+        Double price = Double.valueOf(strPrice);
+        String type = "(T|t)..(E|e)";
+        String expiration = "(E|e)........(N|n)";
+
+        ArrayList<String> temp = parseRawDataIntoStringArray(rawItem);
+      //  ArrayList<Item> myItem = new ArrayList<Item>();
+
+        for (int i = 0; i < temp.size(); i++) {
+            if (findKeyValuePairsInRawItemData(temp.get(i)).equals(null)) {
+                count++;
+                throw new ItemParseException();
+            }
+            else if (findKeyValuePairsInRawItemData(temp.get(i)).size() == 4) {
+                for (int j = 0; j < temp.size(); j++) {
+
+                }
+            }
+        }
+        return null;
     }
 
 
